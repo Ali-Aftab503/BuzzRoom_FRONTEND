5cd657a76e2d344f29fdb9ffa6e4210628a75a15
Ali Aftab <aliaftab3503@gmail.com>
2025-11-16 14:09:20 +0500

    Add TURN servers for video calling

--- ChatApp/FRONTEND/src/components/VideoCall/VideoCall.jsx
+++ ChatApp/FRONTEND/src/components/VideoCall/VideoCall.jsx
@@ -20,9 +20,31 @@ const VideoCall = ({
 
   const configuration = {
     iceServers: [
+      // Google STUN servers
       { urls: 'stun:stun.l.google.com:19302' },
-      { urls: 'stun:stun1.l.google.com:19302' }
-    ]
+      { urls: 'stun:stun1.l.google.com:19302' },
+      { urls: 'stun:stun2.l.google.com:19302' },
+      { urls: 'stun:stun3.l.google.com:19302' },
+      { urls: 'stun:stun4.l.google.com:19302' },
+      
+      // Free public TURN servers (work across different networks)
+      {
+        urls: 'turn:openrelay.metered.ca:80',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      }
+    ],
+    iceCandidatePoolSize: 10
   };
 
   useEffect(() => {
