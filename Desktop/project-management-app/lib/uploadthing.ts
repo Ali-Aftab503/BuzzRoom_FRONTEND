8d9a144b16d611e264ef2819d19615b577cd511d
Ali Aftab <aliaftab3503@gmail.com>
2025-11-23 02:31:31 +0500

    directory corrected

--- Desktop/project-management-app/lib/uploadthing.ts
+++
@@ -1,9 +0,0 @@
-import { generateReactHelpers } from "@uploadthing/react";
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
-
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>();import { generateReactHelpers } from "@uploadthing/react";
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
-
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>();
